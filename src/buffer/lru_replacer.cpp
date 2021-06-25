763c26652cae6f0eabbe19945cf14da01ae91e53
fanming.chen <fanming.chen@wz-inc.com>
2021-06-25 20:20:19 +0800

    1. lru_replacer

--- src/buffer/lru_replacer.cpp
+++ src/buffer/lru_replacer.cpp
@@ -14,16 +14,50 @@
 
 namespace bustub {
 
-LRUReplacer::LRUReplacer(size_t num_pages) {}
+LRUReplacer::LRUReplacer(size_t num_pages) {
+    this->num_pages = num_pages;
+}
 
 LRUReplacer::~LRUReplacer() = default;
 
-bool LRUReplacer::Victim(frame_id_t *frame_id) { return false; }
+bool LRUReplacer::Victim(frame_id_t *frame_id) {
+    std::scoped_lock<std::mutex> lru_lock(lru_mutex);
+    if (cache.empty()) {
+        return false;
+    }
 
-void LRUReplacer::Pin(frame_id_t frame_id) {}
+    *frame_id = cache.back();
+    auto it = --cache.end();
+    frame_id_map.erase(*frame_id);
+    cache.erase(it);
+    return true;
+}
 
-void LRUReplacer::Unpin(frame_id_t frame_id) {}
+void LRUReplacer::Pin(frame_id_t frame_id) {
+    std::scoped_lock<std::mutex> lru_lock(lru_mutex);
+    auto it = frame_id_map.find(frame_id);
+    if (it != frame_id_map.end()) {
+        cache.erase(it->second);
+        frame_id_map.erase(it);
+    }
+}
 
-size_t LRUReplacer::Size() { return 0; }
+void LRUReplacer::Unpin(frame_id_t frame_id) {
+    std::scoped_lock<std::mutex> lru_lock(lru_mutex);
+    auto it = frame_id_map.find(frame_id);
+    if (it != frame_id_map.end()) {
+        return;
+    }
+
+    if (num_pages <= frame_id_map.size()) {
+        frame_id_map.erase(cache.back());
+        cache.erase(--cache.end());
+    }
+
+    cache.push_front(frame_id);
+    frame_id_map[frame_id] = cache.begin();
+}
+
+size_t LRUReplacer::Size() { return cache.size(); }
 
 }  // namespace bustub
